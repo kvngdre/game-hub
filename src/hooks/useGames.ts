f6b5aa22f06b9334b476b05348dbc6aebd824dc8
kvngdre <kennedyugo2@gmail.com>
2026-01-06 07:17:15 -0600

    Show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,26 +23,34 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+
+    setIsLoading(true);
 
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((response) => {
         setGames(response.data.results);
+        setIsLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching games:", error);
 
         if (error instanceof CanceledError) return;
         setError("Failed to fetch games");
+        setIsLoading(false);
       });
+    // .finally(() => {
+    //   setIsLoading(false);
+    // });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
