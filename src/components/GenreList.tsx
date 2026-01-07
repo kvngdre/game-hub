2469060192b845278bd1d14d64a969e3daec0ed6
kvngdre <kennedyugo2@gmail.com>
2026-01-07 07:11:29 -0600

    Add loading spinner and error handling to GenreList component

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,15 +1,24 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { genres } = useGenres();
+  const { genres, isLoading, error } = useGenres();
+
+  if (error) return null;
+
+  if (isLoading) return <Spinner />;
 
   return (
     <List>
       {genres.map((genre) => (
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
-            <Image src={genre.image_background} boxSize="32px" borderRadius={8} />
+            <Image
+              src={getCroppedImageUrl(genre.image_background)}
+              boxSize="32px"
+              borderRadius={8}
+            />
             <Text fontSize="lg">{genre.name}</Text>
           </HStack>
         </ListItem>
